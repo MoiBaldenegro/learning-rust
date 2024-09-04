77973c85d32fdbb92e1fadf6bb09c79e57360605
Moi Baldenegro <124749789+MoiBaldenegro@users.noreply.github.com>
2024-09-04 15:57:19 -0600

    Delete 02-variables/variables/target directory

--- 02-variables/variables/target/debug/deps/variables-f4b709060a6f8104.d
+++
@@ -1,5 +0,0 @@
-/home/moibaldenegro/Documents/rust/Learning_rust/02-variables/variables/target/debug/deps/variables-f4b709060a6f8104: src/main.rs
-
-/home/moibaldenegro/Documents/rust/Learning_rust/02-variables/variables/target/debug/deps/variables-f4b709060a6f8104.d: src/main.rs
-
-src/main.rs:
--- 02-variables/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/home/moibaldenegro/Documents/rust/Learning_rust/02-variables/variables/target/debug/variables: /home/moibaldenegro/Documents/rust/Learning_rust/02-variables/variables/src/main.rs
